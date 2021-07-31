f91fb0acc898ecbe47b8a4b94d80741a0a9740ec
Nick Gerancher <nick@nicksdesk.com>
2021-07-31 09:37:17 -0400

    iniial home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,28 @@
+import './Home.css';
+import { useSpring, animated } from 'react-spring';
+
 const Home = () => {
+    const props = useSpring({
+        to: {
+            opacity: 1,
+        },
+        from: {
+            opacity: 0,
+        }
+    });
+
     return (
-        <div>
-            home
+        <div className="home-container">
+            <animated.section style={props} className="hero slidein-hero">
+                <div className="hero-body">
+                    <p className="title">
+                        Nick's Desk
+                    </p>
+                    <p className="subtitle slidein-title">
+                        Welcome to my desk!
+                    </p>
+                </div>
+            </animated.section>
         </div>
     );
 };
