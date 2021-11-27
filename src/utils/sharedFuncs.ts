a149937908fa4801a10f97012e4e5fa47b24c90a
Nick Gerancher <nick@nicksdesk.com>
2021-11-26 23:26:23 -0500

    updates

--- src/utils/sharedFuncs.ts
+++ src/utils/sharedFuncs.ts
@@ -1,3 +1,9 @@
 export const getEnv = <T>(name: string): T | string => {
     return (process.env[`REACT_APP_${name}`] as unknown) as T;
 };
+
+export const nonmut = (obj: any): any => {
+    return {
+        ...obj,
+    } as const;
+};
